682795f585b61c73c70d00326211e3ebe6a2720d
Daniel Montero <dmonterv@gmail.com>
2021-06-29 12:20:11 -0400

    Agregamos requisito reserva en la carpeta doc

--- misitio/settings.py
+++ misitio/settings.py
@@ -82,7 +82,18 @@ DATABASES = {
     }
 }
 
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'dbadmin',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+"""
 
 
 # Password validation
